6b4827fd7412680ae98f7a03977091f068203e2c
wangyj <wangyj@liepin.com>
2018-01-29 14:48:52 +0800

    support post method

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"go.uber.org/zap"
 )
@@ -12,6 +13,20 @@ func SmsHandle(w http.ResponseWriter, r *http.Request) {
 	appid := vals.Get("appid")
 	tos := vals.Get("tos")
 	content := vals.Get("content")
+
+	if strings.ToUpper(r.Method) == "POST" {
+		r.ParseForm()
+		if appid == "" {
+			appid = r.Form.Get("appid")
+		}
+		if tos == "" {
+			tos = r.Form.Get("tos")
+		}
+		if content == "" {
+			content = r.Form.Get("content")
+		}
+	}
+
 	if appid == "" || tos == "" || content == "" {
 		logger.Error("必选参数不能为空", zap.String("appid", appid), zap.String("tos", tos), zap.String("content", content))
 		fmt.Fprintln(w, "appid 或 tos 或 content 不能为空")
@@ -34,6 +49,22 @@ func MailHandle(w http.ResponseWriter, r *http.Request) {
 	tos := vals.Get("tos")
 	content := vals.Get("content")
 	subject := vals.Get("subject")
+
+	if strings.ToUpper(r.Method) == "POST" {
+		r.ParseForm()
+		if appid == "" {
+			appid = r.Form.Get("appid")
+		}
+		if tos == "" {
+			tos = r.Form.Get("tos")
+		}
+		if content == "" {
+			content = r.Form.Get("content")
+		}
+		if subject == "" {
+			subject = r.Form.Get("subject")
+		}
+	}
 	if appid == "" || tos == "" || content == "" || subject == "" {
 		logger.Error("必选参数不能为空", zap.String("appid", appid), zap.String("tos", tos), zap.String("content", content), zap.String("subject", subject))
 		fmt.Fprintln(w, "appid 或 tos 或 content 不能为空")
@@ -57,6 +88,23 @@ func DingTalkHandle(w http.ResponseWriter, r *http.Request) {
 	tos := vals.Get("tos")
 	content := vals.Get("content")
 	alarmCode := vals.Get("alarmCode")
+
+	if strings.ToUpper(r.Method) == "POST" {
+		r.ParseForm()
+		if appid == "" {
+			appid = r.Form.Get("appid")
+		}
+		if tos == "" {
+			tos = r.Form.Get("tos")
+		}
+		if content == "" {
+			content = r.Form.Get("content")
+		}
+		if alarmCode == "" {
+			alarmCode = r.Form.Get("alarmCode")
+		}
+	}
+
 	if appid == "" || tos == "" || content == "" || alarmCode == "" {
 		logger.Error("必选参数不能为空", zap.String("appid", appid), zap.String("tos", tos), zap.String("content", content), zap.String("alarmCode", alarmCode))
 		fmt.Fprintln(w, "appid 或 tos 或 content 不能为空")
